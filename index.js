1a4a3af5dea16921e156417a8301871045ba4d8a
unknown <martin@MartinN-PC.entelect.local>
2014-06-29 16:40:37 +0200

    Core chomp engine and example complete

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-﻿
-var kwaaitools=
-{
-    apiexpress:require('./lib/apiexpress.js').createApiExpress
-    ,jsonSchema:require('./lib/jsonschema-tools.js')
-    ,helpers:require('./lib/helpers.js')
-    ,mongo:require('./lib/mongo-tools.js')
-    ,crud:require('./lib/crud-tools.js')
-    ,webcrud:require('./lib/webcrud-tools.js')
-    ,tenantTools:require('./lib/tenant-tools.js')
-}
-
-module.exports.tools = kwaaitools;
-
-
+﻿module.exports = require('./lib/chomp.js');
 
 
 
---
+++ testing.js
@@ -0,0 +1,23 @@
+var chomp=require('./lib/chomp.js');
+
+
+chomp.bite(step1,step2)
+chomp.bite(step3)
+
+chomp.run({},function(err,result){
+    var x;
+})
+
+function step1(backpack,res,next){
+    backpack.a=1;
+    next();
+}
+
+function step2(backpack,res,next){
+    backpack.b=2;
+    next();
+}
+
+function step3(backpack,res,next){
+    res(backpack.a+backpack.b);
+}
